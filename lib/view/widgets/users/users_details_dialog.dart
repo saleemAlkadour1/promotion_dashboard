2985e9841e5372cb4150967e5294dce29f9636d6
saleemAlkadour1 <saleemalkadour@gmail.com>
2025-01-22 01:12:09 +0300

    Update

--- lib/view/widgets/users/users_details_dialog.dart
+++ lib/view/widgets/users/users_details_dialog.dart
@@ -1,98 +1,115 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:promotion_dashboard/core/constants/app_colors.dart';
-import 'package:promotion_dashboard/data/model/home/orders/order_model.dart';
+import 'package:promotion_dashboard/data/model/home/transactions/transaction_model.dart';
 
 class UserDetailsDialog extends StatelessWidget {
-  const UserDetailsDialog({super.key, required this.orderModel});
-  final OrderModel orderModel;
+  const UserDetailsDialog({
+    super.key,
+    required this.transactionModel,
+  });
+  final TransactionModel transactionModel;
 
   @override
   Widget build(BuildContext context) {
-    final isMobile = MediaQuery.of(context).size.width < 600;
     return Dialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16.0),
-      ),
+      backgroundColor: Colors.white,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
       child: Container(
-        width: isMobile ? double.infinity : 600,
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16),
+        width: MediaQuery.of(context).size.width * 0.9,
+        constraints: const BoxConstraints(maxWidth: 400),
         child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Header with close button
-            _buildHeader(context),
+            // Header
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Transaction Details',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: GestureDetector(
+                    onTap: () => Get.back(),
+                    child:
+                        const Icon(Icons.close, size: 24, color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+            const Divider(color: Colors.grey),
             const SizedBox(height: 8),
-            const Divider(),
-            Expanded(
+
+            // Details
+            Flexible(
               child: SingleChildScrollView(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const SizedBox(height: 8),
+                    _buildLabelValueRow('Transaction ID',
+                        transactionModel.id.toString(), context),
+                    _buildLabelValueRow(
+                        'Amount',
+                        '${transactionModel.amount} ${transactionModel.currency}',
+                        context),
+                    _buildLabelValueRow('Type', transactionModel.type, context),
+                    _buildLabelValueRow('Category ID',
+                        transactionModel.categoryId.toString(), context),
+                    _buildLabelValueRow(
+                        'User ID', transactionModel.userId.toString(), context),
+                    _buildLabelValueRow(
+                        'Description', transactionModel.description, context),
+                    const SizedBox(height: 12),
 
-                    // Order Details
-                    _buildSectionTitle("Order Info", Icons.shopping_cart),
-                    _buildDetailRow("Order ID", orderModel.id.toString()),
-                    _buildDetailRow("Description", orderModel.description),
-                    _buildDetailRow("Purchase Price", orderModel.purchasePrice),
-                    _buildDetailRow("Sale Price", orderModel.salePrice),
-                    _buildDetailRow("Status", orderModel.status),
-                    _buildDetailRow("Created At", orderModel.createdAt),
-                    const SizedBox(height: 16),
+                    // Dates
+                    _buildLabelValueRow(
+                      'Created At',
+                      transactionModel.createdAt.toIso8601String(),
+                      context,
+                    ),
+                    _buildLabelValueRow(
+                      'Updated At',
+                      transactionModel.updatedAt.toIso8601String(),
+                      context,
+                    ),
+                    const SizedBox(height: 12),
 
-                    // Product Details
-                    _buildSectionTitle(
-                        "Product Info", Icons.production_quantity_limits),
-                    _buildDetailRow("Product Name", orderModel.product.type),
-                    _buildDetailRow("Product Purchase Price",
-                        orderModel.product.purchasePrice),
-                    _buildDetailRow(
-                        "Category", orderModel.product.category.nameEn),
-                    const SizedBox(height: 16),
-
-                    // User Details
-                    _buildSectionTitle("User Info", Icons.person),
-                    _buildDetailRow("User Name", orderModel.user.firstName),
-                    _buildDetailRow("Email", orderModel.user.email),
-                    _buildDetailRow("Phone", orderModel.user.phone),
-                    _buildDetailRow("Wallet Balance",
-                        orderModel.user.walletBalance.toString()),
-                    const SizedBox(height: 16),
-
-                    // Metadata Details
-                    if (orderModel.metadata != null &&
-                        orderModel.metadata is Map) ...[
-                      _buildSectionTitle("Additional Info", Icons.info),
-                      _buildDetailRow(
-                        "Server Name",
-                        orderModel.metadata['server_name'] is String
-                            ? orderModel.metadata['server_name']
-                            : 'N/A',
+                    // User Info
+                    const Text(
+                      'User Info:',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
                       ),
-                      if (orderModel.metadata['data'] != null &&
-                          orderModel.metadata['data'] is Map) ...[
-                        _buildDetailRow(
-                          "Country",
-                          orderModel.metadata['data']['country'] is String
-                              ? orderModel.metadata['data']['country']
-                              : 'N/A',
-                        ),
-                        _buildDetailRow(
-                          "Product",
-                          orderModel.metadata['data']['product'] is String
-                              ? orderModel.metadata['data']['product']
-                              : 'N/A',
-                        ),
-                        _buildDetailRow(
-                          "Operator",
-                          orderModel.metadata['data']['operator'] is String
-                              ? orderModel.metadata['data']['operator']
-                              : 'N/A',
-                        ),
-                      ] else
-                        const SizedBox(),
-                    ] else
-                      const SizedBox()
+                    ),
+                    const SizedBox(height: 8),
+                    _buildLabelValueRow(
+                      'Name',
+                      transactionModel.user.firstName,
+                      context,
+                    ),
+                    _buildLabelValueRow(
+                        'Email', transactionModel.user.email, context),
+                    _buildLabelValueRow(
+                        'Wallet Balance',
+                        transactionModel.user.walletBalance.toString(),
+                        context),
+                    _buildLabelValueRow(
+                      'Role',
+                      transactionModel.user.role,
+                      context,
+                    ),
+                    _buildLabelValueRow(
+                      'Phone',
+                      transactionModel.user.phone,
+                      context,
+                    ),
                   ],
                 ),
               ),
@@ -103,66 +120,34 @@ class UserDetailsDialog extends StatelessWidget {
     );
   }
 
-  Widget _buildHeader(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        const Text(
-          "Order Details",
-          style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
-            color: AppColors.color_064061,
-          ),
-        ),
-        IconButton(
-          icon: const Icon(Icons.close, color: AppColors.color_064061),
-          onPressed: () {
-            Get.back();
-          },
-        ),
-      ],
-    );
-  }
-
-  Widget _buildSectionTitle(String title, IconData icon) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Row(
-        children: [
-          Icon(icon, color: Colors.blueAccent),
-          const SizedBox(width: 8),
-          Text(
-            title,
-            style: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              color: Colors.blueAccent,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildDetailRow(String title, String value) {
+  // Helper Method for Label-Value Rows
+  Widget _buildLabelValueRow(
+    String label,
+    String value,
+    BuildContext context,
+  ) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 4.0),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            "$title: ",
+            '$label: ',
             style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.black87,
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+              color: Colors.grey,
             ),
           ),
           Expanded(
-            flex: 2,
             child: Text(
               value,
-              style: const TextStyle(color: Colors.black54),
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+                color: Colors.black,
+              ),
+              textAlign: TextAlign.end,
             ),
           ),
         ],
